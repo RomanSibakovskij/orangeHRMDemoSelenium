0fa3f9a678d47685fff9d7009b3796f208f3214c
RomanSibakovskij <j4023958@gmail.com>
2024-09-16 17:22:59 +0300

    Add basics for admin change password test

--- src/test/java/com/orangehrm/demo/AdminDashboardPageTest.java
+++ src/test/java/com/orangehrm/demo/AdminDashboardPageTest.java
@@ -4,5 +4,15 @@ import org.junit.jupiter.api.*;
 
 public class AdminDashboardPageTest extends TestMethods{
 
+    //Test 003 -> admin 'change password' test
+    @Test
+    @DisplayName("Change Password as Administrator Test")
+    @Tag("Change_Admin_Password")
+    void adminChangePasswordTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminDashBoardPage adminDashBoardPage = new AdminDashBoardPage(driver);
+        changeAdminPasswordTest();
+    }
     
 }
