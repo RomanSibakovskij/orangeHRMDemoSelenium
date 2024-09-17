c92038a90a7c2d10960a693993b9d80deed6e2f0
RomanSibakovskij <j4023958@gmail.com>
2024-09-17 11:26:59 +0300

    Move admin change password tests to AdminChangePasswordTest class sheet

---
+++ src/test/java/com/orangehrm/demo/AdminChangePasswordTest.java
@@ -0,0 +1,62 @@
+package com.orangehrm.demo;
+
+import org.junit.jupiter.api.*;
+
+public class AdminChangePasswordTest extends TestMethods{
+
+    //Test 003 -> admin 'change password' test
+    @Test
+    @DisplayName("Change Password as Administrator Test")
+    @Tag("Change_Admin_Password")
+    void adminChangePasswordTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminChangePasswordPage adminChangePasswordPage = new AdminChangePasswordPage(driver);
+        changeAdminPasswordTest(adminChangePasswordPage);
+    }
+
+    //Test 003a -> admin login with changed password test (login fails with valid updated password)
+    @Test
+    @DisplayName("Login with Changed Password as Administrator Test")
+    @Tag("Login_New_Admin_Password")
+    void adminChangedPasswordLoginTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminChangePasswordPage adminChangePasswordPage = new AdminChangePasswordPage(driver);
+        changedAdminPasswordLoginTest(adminChangePasswordPage);
+    }
+
+    //Test 003b -> invalid admin 'change password' test -> mismatching old password
+    @Test
+    @DisplayName("Change Password as Administrator Test - Mismatch Old Password")
+    @Tag("Invalid_Change_Admin_Password")
+    void adminChangeMismatchOldPasswordTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminChangePasswordPage adminChangePasswordPage = new AdminChangePasswordPage(driver);
+        changeAdminMismatchOldPasswordTest(adminChangePasswordPage);
+    }
+
+    //Test 003c -> invalid admin 'change password' test -> mismatching new password
+    @Test
+    @DisplayName("Change Password as Administrator Test - Mismatch Confirm Password")
+    @Tag("Invalid_Change_Admin_Password")
+    void adminChangeMismatchNewPasswordTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminChangePasswordPage adminChangePasswordPage = new AdminChangePasswordPage(driver);
+        changeAdminMismatchNewPasswordTest(adminChangePasswordPage);
+    }
+
+    //Test 003d -> admin 'change password' test -> cancel password change
+    @Test
+    @DisplayName("Cancel Password Change as Administrator Test")
+    @Tag("Change_Admin_Password_Cancel")
+    void adminCancelChangePasswordTest(){
+        AdminLoginPage adminLoginPage = new AdminLoginPage(driver);
+        loginAsAdminTest(adminLoginPage);
+        AdminChangePasswordPage adminChangePasswordPage = new AdminChangePasswordPage(driver);
+        cancelChangeAdminPasswordTest(adminChangePasswordPage);
+    }
+
+}
