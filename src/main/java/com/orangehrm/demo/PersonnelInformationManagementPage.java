27681b5ae87433df50c9a82aab7574ccf1681d3f
RomanSibakovskij <j4023958@gmail.com>
2024-09-19 15:53:19 +0300

    Add empty PersonnelInformationManagementPage(PIM) sheet

---
+++ src/main/java/com/orangehrm/demo/PersonnelInformationManagementPage.java
@@ -0,0 +1,13 @@
+package com.orangehrm.demo;
+
+import org.openqa.selenium.*;
+
+public class PersonnelInformationManagementPage extends BasePage{
+
+
+    public PersonnelInformationManagementPage(WebDriver driver) {
+        super(driver);
+    }
+
+    
+}
